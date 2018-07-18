fcf22bbf1f4356753c29e3592d1b88c3a45809db
Shine <1075633455@qq.com>
2018-07-18 11:50:26 +0800

    添加一条bar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import { connect, Dispatch, SubscriptionAPI } from 'dva';
-import { Layout } from 'antd';
 import styled from 'styled-components';
 import { HomeStore } from './Home.model';
 import Footer from '../components/footer/Footer';
 import Top from '../components/top/Top';
+import { List } from 'antd-mobile';
+
+const Item = List.Item;
 
 const Homebox = styled.div`
 	position: relative;
 	min-height: 100vh;
 	.main{
+		text-align: center;
 		min-height: 500px;
 		>div{
-			width: 60%;
+			width: 80%;
 			margin: 0 auto;
 		}
 	}
@@ -37,27 +40,10 @@ export default class Home extends React.PureComponent<HomeProps, HomeState> {
 				<Top />
 				<div className="main">
 					<div>
-						<h1>CSS 书写顺序</h1>
-						<p>
-							位置(position, top, right, bottom, left, z-index)<br />
-							尺寸(display，width, height, padding, margin, overflow)<br />
-							文字(font, line-height, letter-spacing, color, text-align)<br />
-							视觉(cursor, background, border, content, opacity, transform)<br />
-							动画(animation, transition)<br />
-							<a href="https://github.com/twitter/recess/blob/master/lib/lint/strict-property-order.js#L36">
-								完整顺序 (twitter)
-							</a>
-						</p>
-					</div>
-					<div>
-						<h1>日志格式</h1>
-						<p>
-							[F] Fixed #2245: 修复一项 BUG<br />
-							[A] Feature #1190: new feature added. 添加了一项新功能<br />
-							[A] Added #2108: same as feature. 添加了一项新功能<br />
-							[R] Removed #985: 移除<br />
-							[I] Improved #186: 改进/提升<br />
-						</p>
+						<h1>车辆评估</h1>
+						<List>
+							<Item arrow="horizontal" onClick={() => {}}>*选择车型</Item>
+						</List>
 					</div>
 				</div>
 				<Footer />
